fb461d5a6e9a5acdaa460356f74b0ee39bae2a21
1vmc1 <vmc0@mail.ru>
2025-12-16 16:14:48 +0300

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+resource "openstack_compute_instance_v2" "vm" {
+  name            = "lab5-vm"
+  image_name      = var.image_name
+  flavor_name     = var.flavor_name
+  key_pair        = var.keypair
+  security_groups = ["default"]
+
+  network {
+    name = var.network_name
+  }
+}
+
+output "vm_ip" {
+  value = openstack_compute_instance_v2.vm.access_ip_v4
+}
